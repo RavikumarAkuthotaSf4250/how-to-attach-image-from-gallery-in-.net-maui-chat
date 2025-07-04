05c6ac3345f70cd71e30f13f794fc1ec97f8ceaf
MohanrajM-SF4782 <mohanraj.murugan@syncfusion.com>
2025-07-04 10:37:27 +0530

    966014 : Prepare ug content and demo sample for TargetStyle properties - .NET MAUI Chat

---
+++ MauiChat/Helper/DataBase.cs
@@ -0,0 +1,49 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MauiChat
+{
+    public class ImageDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public ImageDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+        }
+
+        public Task<List<ImageModel>> GetImagesAsync()
+        {
+            return _database.Table<ImageModel>().ToListAsync();
+        }
+
+        public Task<ImageModel> GetImageByIdAsync(int id)
+        {
+            return _database.Table<ImageModel>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveImageAsync(ImageModel image)
+        {
+            return _database.InsertAsync(image);
+        }
+
+        public Task<int> DeleteImageAsync(ImageModel image)
+        {
+            return _database.DeleteAsync(image);
+        }
+
+        public Task<int> UpdateImageAsync(ImageModel image)
+        {
+            return _database.UpdateAsync(image);
+        }
+
+        public async void RefreshDataBaseTable()
+        {
+            await _database.DropTableAsync<ImageModel>();
+            _database.CreateTableAsync<ImageModel>().Wait();
+            return;
+        }
+    }
+}
---
+++ MauiChat/ViewModel/ChatViewModel.cs
@@ -0,0 +1,131 @@
+﻿using Syncfusion.Maui.Chat;
+using Syncfusion.Maui.Core;
+using Syncfusion.Maui.Popup;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MauiChat
+{
+    public class ChatViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private ObservableCollection<object> messages;
+
+        private Author currentAuthor;
+
+        private Author Harrison = new Author() { Name = "Harrison", Avatar = "harrison.png" };
+
+        private Author Margaret = new Author() { Name = "Margaret", Avatar = "margaret.png" };
+
+        private Author Stevan = new Author() { Name = "Steven", Avatar = "steven.png" };
+
+        #endregion
+
+        #region Constructor
+        public ChatViewModel()
+        {
+            this.PopupItems = new ObservableCollection<Model>()
+            {
+               new Model(){ ImageSource = "\ue76c", Text="Image" },
+               new Model(){ ImageSource = "\ue7a1", Text="Audio" },
+               new Model(){ ImageSource = "\ue797", Text="Document" },
+               new Model(){ ImageSource = "\ue712", Text="Folder" },
+               new Model(){ ImageSource = "\ue774", Text="Event" },
+            };
+            
+            this.messages = new ObservableCollection<object>();
+            this.currentAuthor = new Author() { Name = "Nancy", Avatar = "nancy.png" };
+            this.GenerateMessages();
+        }
+
+        #endregion
+
+        #region Public Properties
+        public ObservableCollection<object> Messages
+        {
+            get
+            {
+                return this.messages;
+            }
+
+            set
+            {
+                this.messages = value;
+            }
+        }
+
+        public Author CurrentUser
+        {
+            get
+            {
+                return this.currentAuthor;
+            }
+            set
+            {
+                this.currentAuthor = value;
+                RaisePropertyChanged("CurrentUser");
+            }
+        }
+
+        public ObservableCollection<Model> PopupItems { get; set; }
+
+
+        #endregion
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        #region Private Methods
+
+        private void GenerateMessages()
+        {
+            this.messages.Add(new TextMessage()
+            {
+                Author = currentAuthor,
+                Text = "Hi guys, good morning! I'm very delighted to share with you the news that our team is going to launch a new mobile application.",
+                DateTime = DateTime.Now
+            });
+
+            this.messages.Add(new TextMessage()
+            {
+                Author = this.Margaret,
+                Text = "Oh! That's great",
+                DateTime = DateTime.Now
+            });
+
+            this.messages.Add(new TextMessage()
+            {
+                Author = this.Harrison,
+                Text = "That is good news.",
+                DateTime = DateTime.Now
+            });
+
+            this.messages.Add(new TextMessage()
+            {
+                Author = this.Stevan,
+                Text = "What kind of application is it and when are we going to launch?.",
+                DateTime = DateTime.Now
+            });
+        }
+
+        #endregion
+
+        #region Callbacks
+
+        private void RaisePropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        #endregion
+    }
+}
